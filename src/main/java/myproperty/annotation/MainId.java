919858a90576519cb181244d445264236f67e42d
Muyinda Rogers <moverr@gmail.com>
2017-05-06 12:30:32 +0300

    Adding Main IDs

---
+++ src/main/java/myproperty/annotation/MainId.java
@@ -0,0 +1,16 @@
+package myproperty.annotation;
+
+/**
+ * Created by Manny on 5/6/2017.
+ */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value={ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface   MainId {
+
+}
