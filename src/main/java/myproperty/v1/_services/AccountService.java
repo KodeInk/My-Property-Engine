36193f45c5d0466812b108fb7bf2b287fc86464b
Muyinda Rogers <moverr@gmail.com>
2017-06-15 00:12:31 +0300

    6152017_accountService_creating_newAccount

--- src/main/java/myproperty/v1/_services/AccountService.java
+++ src/main/java/myproperty/v1/_services/AccountService.java
@@ -5,11 +5,21 @@
  */
 package myproperty.v1._services;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import myproperty.v1._entities.Account;
 import myproperty.v1._entities.Accounts;
+import myproperty.v1._entities.Contacts;
+import myproperty.v1._entities.Person;
+import myproperty.v1._entities.User;
+import myproperty.v1._entities.responses.PersonResponse;
+import myproperty.v1._entities.responses.UserResponse;
+import myproperty.v1.helper.ContactTypes;
+import myproperty.v1.helper.ParentTypes;
+import myproperty.v1.helper.StatusEnum;
 import myproperty.v1.helper.exception.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,15 +32,25 @@ public class AccountService {
     private String names;
     private String email_address;
     private String password;
+    UserResponse userResponse;
+    PersonResponse personResponse;
+    User user;
+    Contacts contacts;
 
     private static final Logger LOG = Logger.getLogger(AccountService.class.getName());
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PersonService personService;
+
+    @Autowired
+    ContactsService contactsService;
     //TODO:Create Account
-    public void createAccount(Account account) {
+    public void createAccount(Account account) throws Exception {
         //STEP ONE: Create User Username and Password:
         // Check to see that Email Address does not exist ::
-
-
         names = account.getNames();
         email_address = account.getEmail_address();
         password = account.getPassword();
@@ -40,11 +60,40 @@ public class AccountService {
             throw new BadRequestException("Failed because of missing mandatory fields ");
         }
         //Check in the Database to see that there is no active user with this email address ::
-
-
+        List<User> users = userService.checkActiveUserByEmail(account.getEmail_address());
+        if (!users.isEmpty()) {
+            throw new BadRequestException("An Active User with this Email Address Exists in the database");
+        }
+        //create user ::
+        {
+            user = new User();
+            user.setUsername(account.getEmail_address());
+            user.setPassword(account.getPassword());
+            user.setStatus(StatusEnum.PENDING.toString());
+            userResponse = userService.createUser(user);
+        }
 
         //STEP TWO: Create Empty Person [Profile]:
+        {
+            if (userResponse.getId() > 0) {
+                Person person = new Person();
+                user.setId(userResponse.getId());
+                person.setUser(user);
+                person.setNames(account.getNames());
+                personResponse = personService.createPerson(person);
+            }
+        }
         //STEP THREE: Create Contact Information Email Address:
+        {
+            if (personResponse.getId() > 0) {
+                contacts = new Contacts();
+                contacts.setType(ContactTypes.EMAIL.toString());
+                contacts.setDetails(account.getEmail_address());
+                contactsService.createContacts(contacts, ParentTypes.PERSON, personResponse.getId());
+            }
+        }
+
+        //TODO: Send Email to the User and Notify about Account Creation ::
     }
 
     //TODO: Activate Account
